e4673ba6eebb5b055dc29a50310344848aafffaf
Aiden <am2497@cam.ac.uk>
2017-12-01 15:33:48 +0000

    fixed bullet code

--- bullet.lua
+++ bullet.lua
@@ -1,30 +1,45 @@
-function bullet_load()
-  bullets = {}
-  
-  score = 0
-end
+local M = {}
 
-function bullet_update(dt)
-  if love.keyboard.isDown("space") then
-    score = score + 1
-    local bullet = {}
-    bullet.x = 0
-    bullet.y = 0
-    bullet.v = 5
-    table.insert(bullets, bullet)
-  end
-  
+local VELOCITY = 5
+
+local bullets = {}
+local isShooting = false
+
+local function update(dt)
   for i = #bullets, 1, -1 do
     local bullet = bullets[i]
-    bullet.x = bullet.x + 5
+    bullet.x = bullet.x + VELOCITY
   end
 end
+M.update = update
 
-function bullet_draw()
+local function keypressed(key)  
+  if key == "space" then
+    if not isShooting then
+      local bullet = {}
+      bullet.x = 0
+      bullet.y = 0
+      table.insert(bullets, bullet)
+    end
+    isShooting = true
+  end
+end
+M.keypressed = keypressed
+
+local function keyreleased(key)
+  if key == "space" then
+    isShooting = false
+  end
+end
+M.keyreleased = keyreleased
+
+local function draw()
   love.graphics.setColor(255, 255, 255)
   for i = #bullets, 1, -1 do
     local bullet = bullets[i]
     love.graphics.rectangle("fill", bullet.x, bullet.y, 10, 10)
   end
-  love.graphics.print("score: " .. score, 10, 10)
 end
+M.draw = draw
+
+return M
