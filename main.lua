6c6c31299004ab2c146f5fab294c584827de745c
Aiden <am2497@cam.ac.uk>
2017-12-01 12:43:32 +0000

    assets folder and main file

---
+++ main.lua
@@ -0,0 +1,22 @@
+function love.load()
+  player = {}
+  player.image = love.graphics.newImage("assets/images/main_character.png")
+  player.x = 0
+  player.y = 0
+end
+
+function love.update(dt)
+  if love.keyboard.isDown("right") then
+    player.x = player.x + 5
+  elseif love.keyboard.isDown("down") then
+    player.y = player.y + 5
+  elseif love.keyboard.isDown("left") then
+    player.x = player.x - 5
+  elseif love.keyboard.isDown("up") then
+    player.y = player.y - 5
+  end
+end
+
+function love.draw()
+  love.graphics.draw(player.image, player.x, player.y)
+end
