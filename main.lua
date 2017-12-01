4a695ce341ba30a555a4874cbe2a9de68b22a4ab
Aiden <am2497@cam.ac.uk>
2017-12-01 17:20:00 +0000

    clean up

--- main.lua
+++ main.lua
@@ -3,21 +3,24 @@ require "collision"
 local mathFunc = require "mathFunc"
 local physics = require "physics"
 local dust_handler = require "dust"
+local power_handler = require "powertype"
 
 function love.load()
+  WIDTH, HEIGHT = love.graphics.getWidth(), love.graphics.getHeight()
+  
   love.graphics.setFont(love.graphics.newFont("assets/fonts/DejaVuSansMono.ttf", 36))
   
   level = {}
   level.char = "z"
   level.text = love.graphics.newText(love.graphics.getFont(), level.char)
-  level.base = love.graphics.getHeight() - love.graphics.getFont():getHeight(level.char) * 1.5
+  level.base = HEIGHT - love.graphics.getFont():getHeight(level.char) * 1.5
   level.grav = love.graphics.getFont():getHeight(level.char) * 30 --one sec to fall
 
   player = {}
   player.char = "("
   player.text = love.graphics.newText(love.graphics.getFont(), player.char)
   player.x = 0
-  player.y = love.graphics.getHeight()-100
+  player.y = HEIGHT-100
   player.w = love.graphics.getFont():getWidth(player.char)
   player.h = love.graphics.getFont():getHeight(player.char)
 
@@ -30,16 +33,6 @@ end
 function love.update(dt)
   local x = player.x
   local y = player.y
-  
-  -- if love.keyboard.isDown("right") then
-  --   player.x = player.x + 5
-  -- elseif love.keyboard.isDown("down") then
-  --   player.y = player.y + 5
-  -- elseif love.keyboard.isDown("left") then
-  --   player.x = player.x - 5
-  -- elseif love.keyboard.isDown("up") then
-  --   player.y = player.y - 5
-  -- end
 
   -- Player
   player.x = physics.updatePos(dt, player.vx, player.x)
@@ -48,11 +41,12 @@ function love.update(dt)
   player.vy = physics.updateVel(dt, level.grav, player.vy)
   
   --Collision Detection
-  if AABB(player.x, player.y, player.w, player.h, 0, level.base, love.graphics.getWidth(), 0) then
+  if AABB(player.x, player.y, player.w, player.h, 0, level.base, WIDTH, 0) then
     -- player.x = x
     player.y = y
   end
   
+  power_handler.update(dt)
   bullet_handler.update(dt)
   dust_handler.update(dt)
 end
@@ -67,7 +61,8 @@ function love.keypressed(key)
   if key == "up" then
     player.vy = -player.h * 10
   end
-
+  
+  power_handler.keypressed(key)
   bullet_handler.keypressed(key)
 end
 
@@ -83,10 +78,10 @@ function love.draw()
   
   love.graphics.setColor(255, 255, 255)
   love.graphics.draw(player.text, player.x, player.y)
-  local width = love.graphics.getWidth()
-  for i = 0, love.graphics.getWidth(), love.graphics.getFont():getWidth(level.char) do
+  for i = 0, WIDTH, love.graphics.getFont():getWidth(level.char) do
     love.graphics.draw(level.text, i, level.base)
   end
   
+  power_handler.draw()
   bullet_handler.draw()
 end
