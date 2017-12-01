7f5c45113179573de84c4ca66fd1c9830e23552a
Aiden <am2497@cam.ac.uk>
2017-12-01 15:10:12 +0000

    implemented basic floor collision detection

--- main.lua
+++ main.lua
@@ -1,13 +1,20 @@
+require "collision"
+
 function love.load()
   player = {}
   player.image = love.graphics.newImage("assets/images/main_character.png")
   player.x = 0
   player.y = 0
+  player.w = 10
+  player.h = 200
   
   font = love.graphics.newFont("assets/fonts/UbuntuMono-R.ttf", 36)
 end
 
 function love.update(dt)
+  local x = player.x
+  local y = player.y
+  
   -- Player
   if love.keyboard.isDown("right") then
     player.x = player.x + 5
@@ -17,6 +24,12 @@ function love.update(dt)
     player.x = player.x - 5
   elseif love.keyboard.isDown("up") then
     player.y = player.y - 5
+  end
+  
+  --Collision Detection
+  if AABB(player.x, player.y, player.w, player.h, 0, love.graphics.getHeight(), love.graphics.getWidth(), 0) then
+    player.x = x
+    player.y = y
   end
 end
 
